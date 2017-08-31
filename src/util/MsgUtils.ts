24b3c7210997216da21aeb2acfcf51b323455976
Tecker.Yu <tecker_yuknigh@163.com>
2017-08-31 12:08:12 +0800

    Prepare for RegExp helper class

--- src/util/MsgUtils.ts
+++ src/util/MsgUtils.ts
@@ -4,16 +4,20 @@ import {
 } from 'botbuilder';
 
 export class MsgUtils {
-  public static sendAsSay(session:Session, msg:string) {
-    session.say(msg, msg);
-  }
-
-  public static onlyAlpha(msg:string) {
-
+  public static sayAsSend(session:Session, msg:string) {
+    return session.say(msg, msg);
   }
 
   public static getDateFromUtterance(msg:string) : Date | null {
     return EntityRecognizer.parseTime(msg);
+  }
+
+  public static meanYes(msg:string) : boolean {
+    return EntityRecognizer.parseBoolean(msg);
+  }
+
+  public static extract(type:string, msg:string) {
+    // TODO: ues regular expression to extract specify str
   }
 
 }
