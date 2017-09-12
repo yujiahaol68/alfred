df13d684b2beb818e3869be44ea0f78b354421ec
Tecker.Yu <tecker_yuknigh@163.com>
2017-09-12 22:06:26 +0800

    Implement high-order function for basicStep interface

---
+++ src/bot/step/basic.ts
@@ -0,0 +1,69 @@
+import {
+  BasicStepBuilder,
+  BasicStep,
+} from './../../interfaces/step_interfaces';
+import {
+  IDialogWaterfallStep,
+  Prompts,
+} from 'botbuilder';
+
+const askFor:BasicStepBuilder = (stateMent:string, payLoad?:any) => {
+
+  const attachment:IDialogWaterfallStep = (session, args, next) => {
+    Prompts.attachment(session, stateMent);
+  };
+
+  const confirm:IDialogWaterfallStep = (session, args, next) => {
+    Prompts.confirm(session, stateMent);
+  };
+
+  const choose:IDialogWaterfallStep = (session, args, next) => {
+    if (!payLoad) throw new Error('chioce must have payLoad');
+    const { options, listStyle } = payLoad;
+
+    if (options instanceof Array) throw new Error('chioce must be contained in an Array');
+    if (options.length <= 1) throw new Error('chioce payLoad must be an Array that length greater than 1');
+
+    Prompts.choice(session, stateMent, options, { listStyle });
+  };
+
+  const detail:IDialogWaterfallStep = (session, args, next) => {
+    Prompts.text(session, stateMent);
+  };
+
+  const email:IDialogWaterfallStep = (session, args, next) => {
+    // TODO: Wait for customized prompts base on regular expression
+  };
+
+  const num:IDialogWaterfallStep = (session, args, next) => {
+    Prompts.number(session, stateMent);
+  };
+
+  const time:IDialogWaterfallStep = (session, args, next) => {
+    Prompts.time(session, stateMent);
+  };
+
+  const location:IDialogWaterfallStep = (session, args, next) => {
+    // TODO: Use botBuilder-location to implement
+  };
+
+  const phoneNumber:IDialogWaterfallStep = (session, args, next) => {
+    // TODO: Wait for customized prompts base on regular expression
+  };
+
+  return {
+    attachment,
+    confirm,
+    choose,
+    detail,
+    email,
+    num,
+    location,
+    phoneNumber,
+    time,
+  };
+};
+
+export const Basic:BasicStep = {
+  askFor,
+};
--- src/interfaces/step_interfaces.ts
+++ src/interfaces/step_interfaces.ts
@@ -2,18 +2,24 @@ import {
   IDialogWaterfallStep,
 } from 'botbuilder';
 
+export interface BasicStepBuilder {
+  (stateMent:string, payLoad?:any) : RequestStep;
+}
+
 export interface BasicStep {
-  askFor:RequestStep;
+  askFor:BasicStepBuilder;
 }
 
 // TODO: high order or builder class to inject config of the step
 
 interface RequestStep {
+  attachment:IDialogWaterfallStep;
   confirm:IDialogWaterfallStep;
-  number:IDialogWaterfallStep[];
-  time:IDialogWaterfallStep[];
-  location:IDialogWaterfallStep[];
-  detail:IDialogWaterfallStep[];
-  phoneNumber:IDialogWaterfallStep[];
-  email:IDialogWaterfallStep[];
+  choose:IDialogWaterfallStep;
+  num:IDialogWaterfallStep;
+  time:IDialogWaterfallStep;
+  location:IDialogWaterfallStep;
+  detail:IDialogWaterfallStep;
+  phoneNumber:IDialogWaterfallStep;
+  email:IDialogWaterfallStep;
 }
